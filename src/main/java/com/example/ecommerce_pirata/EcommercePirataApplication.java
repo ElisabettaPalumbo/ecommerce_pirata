19b4d4350e6fb331f7c08a156a1cb1e15c4aa151
ElisabettaPalumbo <elisabettapalumbo200@gmail.com>
2024-05-09 17:45:51 +0200

    Initial commit

---
+++ src/main/java/com/example/ecommerce_pirata/EcommercePirataApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_pirata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePirataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePirataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce_pirata/EcommercePirataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_pirata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePirataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
